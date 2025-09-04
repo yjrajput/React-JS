3cedb94163f5407b519f2f4217b570bfbb65ab4c
yrajput <yuvrajrathore142@gmail.com>
2025-09-04 18:40:58 +0530

    Cateogry component

--- beginner-level-project/06-advance-searching-project/src/Sidebar/Category/Category.jsx
+++ beginner-level-project/06-advance-searching-project/src/Sidebar/Category/Category.jsx
@@ -2,7 +2,32 @@ import React from 'react'
 import './Category.css'
 const Category = () => {
   return (
-    <div>Category</div>
+    <div>
+      <h2 className='sidebar-title'>Category</h2>
+
+      <div>
+        <label className='sidebar-lable-container'>
+          <input type="radio" name='test' />
+          <span className='checkmarks'></span>All
+        </label>
+        <label className='sidebar-lable-container'>
+          <input type="radio" name='test' />
+          <span className='checkmarks'></span>Sneakers
+        </label>
+        <label className='sidebar-lable-container'>
+          <input type="radio" name='test' />
+          <span className='checkmarks'></span>Flats
+        </label>
+        <label className='sidebar-lable-container'>
+          <input type="radio" name='test' />
+          <span className='checkmarks'></span>Sandals
+        </label>
+        <label className='sidebar-lable-container'>
+          <input type="radio" name='test' />
+          <span className='checkmarks'></span>Heels
+        </label>
+      </div>
+    </div>
   )
 }
 
