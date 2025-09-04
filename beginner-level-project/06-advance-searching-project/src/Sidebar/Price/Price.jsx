6342aed7af87b570a449f8f61275e64f5967b1bf
yrajput <yuvrajrathore142@gmail.com>
2025-09-04 19:24:09 +0530

    Price Section

--- beginner-level-project/06-advance-searching-project/src/Sidebar/Price/Price.jsx
+++ beginner-level-project/06-advance-searching-project/src/Sidebar/Price/Price.jsx
@@ -2,7 +2,30 @@ import React from 'react'
 import './Price.css'
 const Price = () => {
   return (
-    <div>Price</div>
+    <div className='ml'>
+      <h2 className='sidebar-title  price-title'>Price</h2>
+
+      <label className='sidebar-lable-container'>
+        <input type="radio" name='text2'/>
+        <span className='checkmarks'></span>All
+      </label>
+      <label className='sidebar-lable-container'>
+        <input type="radio" name='text2'/>
+        <span className='checkmarks'></span>$0-$50
+      </label>
+      <label className='sidebar-lable-container'>
+        <input type="radio" name='text2'/>
+        <span className='checkmarks'></span>$50-$100
+      </label>
+      <label className='sidebar-lable-container'>
+        <input type="radio" name='text2'/>
+        <span className='checkmarks'></span>$100-$150
+      </label>
+      <label className='sidebar-lable-container'>
+        <input type="radio" name='text2'/>
+        <span className='checkmarks'></span>$150-$200
+      </label>
+    </div>
   )
 }
 
